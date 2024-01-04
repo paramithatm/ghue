f4fccce48829502584a93945117aaf7694d3dc9d
Paramitha <paramithatm@gmail.com>
2024-01-04 17:22:33 +0700

    Initial Commit

---
+++ git-user-explorer/git_user_explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_user_explorerApp.swift
+//  git-user-explorer
+//
+//  Created by Paramitha on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct git_user_explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
