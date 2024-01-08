f7ef4a23d97ef24133b905e81267e22c684264b0
Paramitha <paramithatm@gmail.com>
2024-01-08 13:10:43 +0700

    adjust padding

--- git-user-explorer/View/User/UserCellView.swift
+++ git-user-explorer/View/User/UserCellView.swift
@@ -15,32 +15,22 @@ struct UserCellView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            HStack(spacing: 8) {
+            HStack(spacing: 16) {
                 AsyncImage(url: URL(string: avatarUrl)) { image in
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 50)
                         .cornerRadius(25)
-                } placeholder: {
+                }
+            placeholder: {
                     ProgressView()
                         .frame(width: 50, height: 50)
                 }
 
-                    
                 Text(name)
-                    .foregroundStyle(Color.paleBlue2)
                 Spacer(minLength: 16)
-                HStack {
-                    Image(systemName: "plus")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                }
             }
         }
     }
 }
-
-//#Preview {
-//    UserCellView(name: "Woohoo", avatarUrl: "")
-//}
